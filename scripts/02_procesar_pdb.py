f2b5c0c05f0d8b3393648937681414156078b398
AndoniM12 <andonim12@gmail.com>
2026-07-21 12:45:24 +0200

    Mejora de cálculo de RMSD

--- scripts/02_procesar_pdb.py
+++ scripts/02_procesar_pdb.py
@@ -7,6 +7,7 @@ El script:
 - conserva únicamente la proteína y el ligando de interés,
 - elimina el resto de moléculas,
 - alinea todas las estructuras respecto a la referencia 7AAA.pdb
+- devuelve información sobre el alineamiento
 
 Las estructuras procesadas se guardan en structures/processed.
 """
@@ -16,6 +17,8 @@ import os
 import pandas as pd
 import subprocess
 import tempfile
+import numpy as np
+import re
 
 INPUT_FOLDER = "structures/raw"
 OUTPUT_FOLDER = "structures/processed"
@@ -23,6 +26,7 @@ reference_pdb = "structures/processed/7AAA_chainA.pdb"
 
 df = pd.read_csv("info_adicional/tabla_pdb.csv")
 ligandos_por_pdb = dict(zip(df["PDB"], df["Código_ligando"]))
+rmsd_values = []
 
 
 def clean_pdb(input_pdb, output_pdb):
@@ -62,12 +66,18 @@ def align_pdb(reference_pdb, pdb):
             f.write(script)
             script_name = f.name
 
-        subprocess.run(
+        result = subprocess.run(
             ["pymol", "-cq", script_name],
-            check=True
+            check=True,
+            capture_output=True,
+            text=True
         )
 
         os.remove(script_name)
+
+        match = re.search(r"Executive:\s*RMSD\s*=\s*(\d+\.\d+)", result.stdout)        
+        
+        return float(match.group(1))
 
 if __name__ == "__main__":
 
@@ -100,6 +110,15 @@ if __name__ == "__main__":
 
         clean_pdb(input_path, output_path)
 
-        align_pdb(reference_pdb, os.path.join(OUTPUT_FOLDER, f"{code[:4]}_chainA.pdb"))
+        rmsd = align_pdb(reference_pdb, os.path.join(OUTPUT_FOLDER, f"{code[:4]}_chainA.pdb"))
 
+        rmsd_values.append(rmsd)
+        
         print(f"  -> Guardado: {output_path}")
+    
+    print("\n===== Resumen RMSD =====")
+    print(f"Estructuras alineadas contra la referencia : {len(rmsd_values)}")
+    print(f"RMSD medio            : {np.mean(rmsd_values):.3f} Å")
+    print(f"Desviación estándar   : {np.std(rmsd_values):.3f} Å")
+    print(f"RMSD mínimo           : {np.min(rmsd_values):.3f} Å")
+    print(f"RMSD máximo           : {np.max(rmsd_values):.3f} Å")

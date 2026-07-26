800cae2f34ff2cf47fe5f43cf8d96b032e770689
AndoniM12 <andonim12@gmail.com>
2026-07-26 21:28:01 +0200

    Extracción de ligandos para bd

--- scripts/02_procesar_pdb.py
+++ scripts/02_procesar_pdb.py
@@ -8,6 +8,7 @@ El script:
 - elimina el resto de moléculas,
 - alinea todas las estructuras respecto a la referencia 7AAA.pdb
 - devuelve información sobre el alineamiento
+- extrae el ligando de los Validation set y los guarda en la base de datos sdf
 
 Las estructuras procesadas se guardan en structures/processed.
 """
@@ -23,6 +24,7 @@ import re
 INPUT_FOLDER = "structures/raw"
 OUTPUT_FOLDER = "structures/processed"
 reference_pdb = "structures/processed/7AAA_chainA.pdb"
+ligand_list = "info_adicional/validation_set_db.sdf"
 
 df = pd.read_csv("info_adicional/tabla_pdb.csv")
 ligandos_por_pdb = dict(zip(df["PDB"], df["Código_ligando"]))
@@ -79,6 +81,47 @@ def align_pdb(reference_pdb, pdb):
         
         return float(match.group(1))
 
+def extract_ligand(pdb, ligand):
+
+    with tempfile.NamedTemporaryFile(
+            mode="w",
+            suffix=".sdf",
+            delete=False,
+        ) as tmp:
+            output_sdf = tmp.name
+
+    script = f"""
+        load {pdb}, prot
+
+        select lig, resn {ligand}
+
+        save {output_sdf}, lig
+
+        quit
+    """
+
+    with tempfile.NamedTemporaryFile(
+        mode="w",
+        suffix=".pml",
+        delete=False
+    ) as f:
+
+        f.write(script)
+        script_name = f.name
+
+    subprocess.run(
+        ["pymol", "-cq", script_name],
+        check=True,
+        capture_output=True,
+        text=True
+    )
+
+    with open(output_sdf, "r") as infile, open(ligand_list, "a") as outfile:
+        outfile.write(infile.read())
+
+    os.remove(output_sdf)
+    os.remove(script_name)
+
 if __name__ == "__main__":
 
     pdb_files = os.listdir(INPUT_FOLDER)
@@ -87,7 +130,7 @@ if __name__ == "__main__":
         pdb_files.remove("PARP1_AF_model.pdb")
 
     if "7AAA" in pdb_files:
-        pdb_files.remove("PARP1_AF_model.pdb")
+        pdb_files.remove("7AAA.pdb")
 
     print(f"Se han encontrado {len(pdb_files)} estructuras para procesar.\n")
 
@@ -95,26 +138,39 @@ if __name__ == "__main__":
         os.mkdir(OUTPUT_FOLDER)
         print(f"Directorio creado: {OUTPUT_FOLDER}")
 
+    print(f"Procesando 7AAA...")
+
     clean_pdb(f"{INPUT_FOLDER}/7AAA.pdb", reference_pdb)
 
-    for code in pdb_files:
+    for _, row in df.iterrows():
 
-        input_path = os.path.join(INPUT_FOLDER, f"{code[:4]}.pdb")
-        output_path = os.path.join(OUTPUT_FOLDER, f"{code[:4]}_chainA.pdb")
+        code = row["PDB"]
+        ligand = row["Código_ligando"]
+        group = row["Grupo de estudio"]
+        download = row["Descarga_pdb"]
 
-        if os.path.exists(output_path):
-            print(f"  -> {code} ya procesado. Se omite.")
-            continue
+        if download == "Sí":
 
-        print(f"Procesando {code}...")
+            input_path = os.path.join(INPUT_FOLDER, f"{code}.pdb")
+            output_path = os.path.join(OUTPUT_FOLDER, f"{code}_chainA.pdb")
 
-        clean_pdb(input_path, output_path)
+            if os.path.exists(output_path):
+                print(f"  -> {code} ya procesado. Se omite.")
+                continue
 
-        rmsd = align_pdb(reference_pdb, os.path.join(OUTPUT_FOLDER, f"{code[:4]}_chainA.pdb"))
+            print(f"Procesando {code}...")
 
-        rmsd_values.append(rmsd)
-        
-        print(f"  -> Guardado: {output_path}")
+            clean_pdb(input_path, output_path)
+
+            rmsd = align_pdb(reference_pdb, output_path)
+
+            rmsd_values.append(rmsd)
+
+            if group == "Validation set":
+
+                extract_ligand(output_path,ligand)
+
+            print(f"  -> Guardado en: {output_path}")
     
     print("\n===== Resumen RMSD =====")
     print(f"Estructuras alineadas contra la referencia : {len(rmsd_values)}")
